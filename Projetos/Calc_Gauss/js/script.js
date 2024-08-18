fb5565576ff234f806f19790149eae7c2ab47169
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-08-18 14:36:14 -0300

    Revert "Update script.js"
    This reverts commit 77ce27508b422a9c1938cc47deb41958ed4ecd5d.

--- Projetos/Calc_Gauss/js/script.js
+++ Projetos/Calc_Gauss/js/script.js
@@ -1,7 +1,6 @@
 var n1 = document.getElementById("n1")
 var n2 = document.getElementById("n2")
 var bt = document.getElementById("bt")
-var btLimpar = document.getElementById("btLimpar")
 
 function gauss() {
     var num1 = Number(n1.value)
@@ -23,21 +22,16 @@ function gauss() {
         meio = vetor.length / 2
         res = (num1 + num2) * meio
         console.log(res)
-        document.getElementById("res").innerText = "A soma dos números de " + num1.toLocaleString("pt-BR") + " à " + num2.toLocaleString("pt-BR") + " é: " + res.toLocaleString("pt-BR")
+        document.getElementById("res").innerText = `A soma dos números de ${num1} à ${num2} é: ${res}`
         return
-    } else if(vetor.length % 2 != 0) {
+    } else {
         meio = (vetor.length / 2) - 0.5
         let res = ((num1 + num2) * meio) + Math.floor(vetor.length / 2) + 1
         console.log(res)
-        document.getElementById("res").innerText =  "A soma dos números de " + num1.toLocaleString("pt-BR") + " à " + num2.toLocaleString("pt-BR") + " é: " + res.toLocaleString("pt-BR")
+        document.getElementById("res").innerText =  `A soma dos números de ${num1} à ${num2} é: ${res}`
         return
     }
 }
 
-function limpar() {
-    document.getElementById("res").innerText = ""
-}
-
 bt.addEventListener("click", gauss)
-btLimpar.addEventListener("click", limpar)
 
