c95af7871dd0d79c04218d05e5fa930868d8f1ac
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-08-18 14:42:19 -0300

    btLimpar add

--- Projetos/Calc_Gauss/js/script.js
+++ Projetos/Calc_Gauss/js/script.js
@@ -1,6 +1,7 @@
 var n1 = document.getElementById("n1")
 var n2 = document.getElementById("n2")
 var bt = document.getElementById("bt")
+var btLimpar = document.getElementById("btLimpar")
 
 function gauss() {
     var num1 = Number(n1.value)
@@ -18,20 +19,25 @@ function gauss() {
     }
 
     //Verificador
-    if (vetor.length % 2 === 0) {      
+    if (vetor.length % 2 == 0) {      
         meio = vetor.length / 2
         res = (num1 + num2) * meio
         console.log(res)
-        document.getElementById("res").innerText = `A soma dos números de ${num1} à ${num2} é: ${res}`
+        document.getElementById("res").innerText = "A soma dos números de " + num1.toLocaleString("pt-BR") + " à "+ num2.toLocaleString("pt-BR") + " é: "+ res.toLocaleString("pt-BR")
         return
-    } else {
+    } else if(vetor.length % 2 == 1) {
         meio = (vetor.length / 2) - 0.5
         let res = ((num1 + num2) * meio) + Math.floor(vetor.length / 2) + 1
         console.log(res)
-        document.getElementById("res").innerText =  `A soma dos números de ${num1} à ${num2} é: ${res}`
+        document.getElementById("res").innerText =  "A soma dos números de " + num1.toLocaleString("pt-BR") + " à "+ num2.toLocaleString("pt-BR") + " é: "+ res.toLocaleString("pt-BR")
         return
     }
 }
 
+function limpar() {
+    document.getElementById("res").innerText = ""
+}
+
 bt.addEventListener("click", gauss)
+btLimpar.addEventListener("click", limpar)
 
