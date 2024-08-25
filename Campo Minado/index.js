832105c71e1f9ac0f5f2151b00676d8910e718af
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-08-25 19:47:10 -0300

    fim do projeto

---
+++ Campo Minado/index.js
@@ -0,0 +1,12 @@
+const {createApp} = Vue
+createApp ({
+    data() {
+        return {
+            bloco: 'dasdsas',
+            teste: 0
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
