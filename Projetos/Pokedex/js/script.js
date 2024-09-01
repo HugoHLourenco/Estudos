7a9df175a3b0865e6c9adc882cf3452a4fc76aae
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-01 14:46:58 -0300

    video encerrado

--- Projetos/Pokedex/js/script.js
+++ Projetos/Pokedex/js/script.js
@@ -1,28 +1,43 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
+
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const btnVolt = document.querySelector('.btn-volt');
+const btnProx = document.querySelector('.btn-prox');
+
+let numPoke = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    if(APIResponse.status === 200) {
-        const data =  await APIResponse.json();
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
         return data;
     }
 
 }
 
-const renderPokemon = async(pokemon) => {
+const renderPokemon = async (pokemon) => {
+    pokemonNumber.innerHTML = ""
+    pokemonName.innerHTML = "Buscando..."
+
     const data = await fetchPokemon(pokemon);
 
     if (data) {
+        pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
         input.value = "";
+
+        numPoke = data.id;
+    } else {
+        pokemonName.innerHTML = "???"
+        pokemonNumber.innerHTML = "???"
+        pokemonImage.style.display = 'none';
     }
 }
 
@@ -31,4 +46,17 @@ form.addEventListener('submit', (event) => {
 
     renderPokemon(input.value.toLowerCase());
 });
+
+btnVolt.addEventListener('click', () => {
+    if (numPoke > 1) {
+        numPoke -= 1;
+        renderPokemon(numPoke);
+    }
+});
+btnProx.addEventListener('click', () => {
+    numPoke += 1;
+    renderPokemon(numPoke);
+});
+
+renderPokemon(numPoke);
 
