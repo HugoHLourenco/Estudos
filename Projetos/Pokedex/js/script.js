1b054661835bfbcb65872fcde74b48a694f11d72
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-09-01 19:39:13 -0300

    gen 6 Add

--- Projetos/Pokedex/js/script.js
+++ Projetos/Pokedex/js/script.js
@@ -13,7 +13,7 @@ const btnGeracao = document.querySelector('.btn-geracao')
 let numPoke = 1;
 let imagem = "";
 let shiny = false;
-let geracao = "0";
+let geracao = "";
 
 // Busca pelos pokemons ----------------------------------------
 const fetchPokemon = async (pokemon) => {
@@ -33,7 +33,37 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
     
     switch (geracao) {
-        // Gen 5 -----------------------------------------------
+        // Gen 6 ------------------------------------------------
+        case "6":
+            if (data.id <= 721 && shiny === true) {
+                pokemonImage.style.display = 'block';
+                pokemonName.innerHTML = data.name;
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = data['sprites']['versions']['generation-vi']['x-y']['front_shiny'];
+        
+                input.value = ""; 
+        
+                imagem = data['sprites']['versions']['generation-vi']['x-y']['front_default'];
+        
+                numPoke = data.id;
+            } else if (data.id <= 721 && shiny === false) {
+                pokemonImage.style.display = 'block';
+                pokemonName.innerHTML = data.name;
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = data['sprites']['versions']['generation-vi']['x-y']['front_default'];
+        
+                input.value = "";
+        
+                imagem = data['sprites']['versions']['generation-vi']['x-y']['front_default'];
+        
+                numPoke = data.id;
+            } else {
+                pokemonNumber.innerHTML = "???"
+                pokemonName.innerHTML = "MissingNO."
+                pokemonImage.src = "../images/missigno.png"
+            }
+            break;
+        // Gen 5 ---------------------------------------
         case "5":
             if (data.id <= 649 && shiny === true) {
                 pokemonImage.style.display = 'block';
@@ -57,9 +87,13 @@ const renderPokemon = async (pokemon) => {
                 imagem = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         
                 numPoke = data.id;
-            } 
+            } else {
+                pokemonNumber.innerHTML = "???"
+                pokemonName.innerHTML = "MissingNO."
+                pokemonImage.src = "../images/missigno.png"
+            }
             break
-        // Gen 4
+        // Gen 4 -------------------------------------
         case "4":
             if (data.id <= 493 && shiny === true) {
                 pokemonImage.style.display = 'block';
@@ -88,8 +122,67 @@ const renderPokemon = async (pokemon) => {
                 pokemonName.innerHTML = "MissingNO."
                 pokemonImage.src = "../images/missigno.png"
             }
-            break
-        // Gen 1 -----------------------------------------------
+            break;
+        // Gen 3 --------------------------------------------
+        case "3":
+            if (data.id <= 385 && shiny === true) {
+                pokemonImage.style.display = 'block';
+                pokemonName.innerHTML = data.name;
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = data['sprites']['versions']['generation-iii']['emerald']['front_shiny'];
+        
+                input.value = ""; 
+        
+                imagem = data['sprites']['versions']['generation-iii']['emerald']['front_default'];
+        
+                numPoke = data.id;
+            } else if (data.id <= 385 && shiny === false) {
+                pokemonImage.style.display = 'block';
+                pokemonName.innerHTML = data.name;
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = data['sprites']['versions']['generation-iii']['emerald']['front_default'];
+        
+                input.value = "";
+        
+                imagem = data['sprites']['versions']['generation-iii']['emerald']['front_default'];
+        
+                numPoke = data.id;
+            } else {
+                pokemonNumber.innerHTML = "???"
+                pokemonName.innerHTML = "MissingNO."
+                pokemonImage.src = "../images/missigno.png"
+            }
+            break;
+        // Gen 2 ---------------------------------------------
+        case "2":
+            if (data.id <= 251 && shiny === true) {
+                pokemonImage.style.display = 'block';
+                pokemonName.innerHTML = data.name;
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = data['sprites']['versions']['generation-ii']['crystal']['front_shiny'];
+        
+                input.value = ""; 
+        
+                imagem = data['sprites']['versions']['generation-ii']['crystal']['front_default'];
+        
+                numPoke = data.id;
+            } else if (data.id <= 251 && shiny === false) {
+                pokemonImage.style.display = 'block';
+                pokemonName.innerHTML = data.name;
+                pokemonNumber.innerHTML = data.id;
+                pokemonImage.src = data['sprites']['versions']['generation-ii']['crystal']['front_default'];
+        
+                input.value = "";
+        
+                imagem = data['sprites']['versions']['generation-ii']['crystal']['front_default'];
+        
+                numPoke = data.id;
+            } else {
+                pokemonNumber.innerHTML = "???"
+                pokemonName.innerHTML = "MissingNO."
+                pokemonImage.src = "../images/missigno.png"
+            }
+        // Gen 1 ---------------------------------------------
         case "1":
             if(data.id < 152) {
                 pokemonImage.style.display = 'block';
@@ -113,13 +206,14 @@ const renderPokemon = async (pokemon) => {
     
 }
 
-// Métodeos ----------------------------------------------------
+// Form --------------------------------------------------
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     renderPokemon(input.value.toLowerCase());
 });
 
+// Volts -------------------------------------------------
 btnVolt.addEventListener('click', async () => {
     if (numPoke > 1) {
         numPoke -= 1;
@@ -127,8 +221,15 @@ btnVolt.addEventListener('click', async () => {
     }
 });
 
+// Proximo ------------------------------------------------
 btnProx.addEventListener('click', async () => {
         switch (geracao) {
+            case "6":
+                if (numPoke < 721){
+                    numPoke += 1;
+                    renderPokemon(numPoke);
+                }
+                break;
             case "5":
                 if (numPoke < 649){
                     numPoke += 1;
@@ -141,11 +242,47 @@ btnProx.addEventListener('click', async () => {
                     renderPokemon(numPoke);
                 }
                 break;
+            case "3":
+                if (numPoke < 385){
+                    numPoke += 1;
+                    renderPokemon(numPoke);
+                }
+                break;
+            case "2":
+                if (numPoke < 251){
+                    numPoke += 1;
+                    renderPokemon(numPoke);
+                }
+                break;
+            case "1":
+                if (numPoke < 151){
+                    numPoke += 1;
+                    renderPokemon(numPoke);
+                }
+                break;
         }
 });
 
+// Botão dos Shinys ----------------------------------------
 btnShiny.addEventListener('click', async (pokemon) => {
     switch(geracao) {
+        case "6":
+            if (pokemonImage.src !== imagem) {
+                pokemonImage.src = imagem
+                shiny = false;
+                btnShiny.style.color = "red";
+                return shiny;
+            } 
+            else {
+                const imgShiny = await fetchPokemon(numPoke);
+                if (imgShiny) {
+                    pokemonImage.src = imgShiny['sprites']['versions']['generation-vi']['x-y']['front_shiny'];
+                    shiny = true;
+                    btnShiny.style.color = "green";
+                    return shiny;   
+                }
+            }    
+            break;
         case "5":
             if (pokemonImage.src !== imagem) {
                 pokemonImage.src = imagem
@@ -180,25 +317,54 @@ btnShiny.addEventListener('click', async (pokemon) => {
                 }
             }
             break
-        case "2":
-
+        case "3":
+            if (pokemonImage.src !== imagem) {
+                pokemonImage.src = imagem
+                shiny = false;
+                btnShiny.style.color = "red";
+                return shiny;
+            } 
+            else {
+                const imgShiny = await fetchPokemon(numPoke);
+                if (imgShiny) {
+                    pokemonImage.src = imgShiny['sprites']['versions']['generation-iii']['emerald']['front_shiny'];
+                    shiny = true;
+                    btnShiny.style.color = "green";
+                    return shiny;   
+                }
+            }
             break
-        case "1":
-            
+        case "2":
+            if (pokemonImage.src !== imagem) {
+                pokemonImage.src = imagem
+                shiny = false;
+                btnShiny.style.color = "red";
+                return shiny;
+            } 
+            else {
+                const imgShiny = await fetchPokemon(numPoke);
+                if (imgShiny) {
+                    pokemonImage.src = imgShiny['sprites']['versions']['generation-ii']['crystal']['front_shiny'];
+                    shiny = true;
+                    btnShiny.style.color = "green";
+                    return shiny;   
+                }
+            }
             break
     }
 
 })
 
+// Mudar Geração --------------------------------------------
 btnGeracao.addEventListener('click', () => {
     const selectbox = document.getElementById('geracao');
        
     geracao = selectbox.value;
-    alert(geracao)
+    renderPokemon(numPoke);
 
     return geracao;
 })
 
 renderPokemon(numPoke);
-geracao = "4";
+geracao = "5";
 
