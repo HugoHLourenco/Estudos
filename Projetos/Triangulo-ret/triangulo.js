7127f357523baf3c641cb27ce5480d5709f86e9e
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-14 15:55:08 -0300

    Update triangulo.js

--- Projetos/Triangulo-ret/triangulo.js
+++ Projetos/Triangulo-ret/triangulo.js
@@ -1,1 +1,10 @@
-let base = 4
+
+function imprimirTrianguloRetangulo(altura) {
+    for (let i = 1; i <= altura; i++) {
+        console.log("*".repeat(i));
+    }
+}
+
+// Exemplo: Triângulo com altura 5
+imprimirTrianguloRetangulo(5);
+
