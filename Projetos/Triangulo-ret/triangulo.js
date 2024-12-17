9ffd23ea48b9930221a04eeda01ab1dd33417dd7
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-17 20:32:02 -0300

    Update triangulo.js

--- Projetos/Triangulo-ret/triangulo.js
+++ Projetos/Triangulo-ret/triangulo.js
@@ -91,15 +91,20 @@ const areaEsf = (r) => {
 // Sequência de Fibonacci -----------------------------------------------------------------------------------------------------------------------
 
 const fibonacci = (n) => {
-    let num = 1
+    let num1 = 0
+    let num2 = 1
+    let numAtual = 0
     let seq = []
 
     for(let i = 0; i < n; i++) {
-        num += num
-        seq += `${num}; `
+        numAtual = num1 + num2
+        num1 = num2
+        num2 = numAtual
+
+        seq += `${numAtual}; `
     }
-    console.log(`A sequência de Fibonacci até o ${n}° número é: \n
-                ${seq}`)
+    console.log(`A sequência de Fibonacci até o ${n}º número é: \n
+    ${seq}`)
 }
 
-fibonacci(2)
+fibonacci(100)
