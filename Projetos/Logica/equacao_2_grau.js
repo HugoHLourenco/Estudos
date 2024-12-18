836a055c9bcecb6f720e465d33d09615701d7c4a
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-18 20:07:23 -0300

    Update equacao_2_grau.js

--- Projetos/Logica/equacao_2_grau.js
+++ Projetos/Logica/equacao_2_grau.js
@@ -6,10 +6,11 @@ const calculoQuadratico = (a, b, c) => {
         let x1 = ((b * -1) + Math.sqrt(res)) / (2 * a)
         let x2 = ((b * -1) - Math.sqrt(res)) / (2 * a)
 
-        console.log(``)
+        console.log(`As raizes de ${a}X² ${b}X ${c} são: 
+            x1 = ${x1} e x2 = ${x2}`)
     }
 
-    console.log(res)
+    // console.log(res)
 }
 
 calculoQuadratico(2, -4, -6)
