aaa8aba0386c1eec9dccbb47535d6f908fad246c
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-18 20:11:06 -0300

    Update equacao_2_grau.js

--- Projetos/Logica/equacao_2_grau.js
+++ Projetos/Logica/equacao_2_grau.js
@@ -2,14 +2,15 @@ const calculoQuadratico = (a, b, c) => {
     var res = b * b
     res -= (4 * a * c)
 
-
     if (res >= 0) {
         let x1 = ((b * -1) + Math.sqrt(res)) / (2 * a)
         let x2 = ((b * -1) - Math.sqrt(res)) / (2 * a)
 
         console.log(`As raizes de ${a}X² ${b}X ${c} são: 
             x1 = ${x1} e x2 = ${x2}`)
+    } else {
+        console.log("Não há raizes para estas expressão")
     }
 }
 
-calculoQuadratico(2, -4, -6)
+calculoQuadratico(0, -4, -6)
