42702f66d8e735d9ff4797191a505536c8577d35
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-18 19:28:02 -0300

    Create volumeEsfera.js

---
+++ Projetos/Logica/volumeEsfera.js
@@ -0,0 +1,15 @@
+const volEsfera = (r) => {
+    let res = r
+
+    Math.pow(res ,3)
+
+    res * 4
+
+    res / 3
+
+    res * 3.14159
+
+    console.log(`Uma esfera de raio(${r}) tem volume de ${res} cm³`)
+} 
+
+volEsfera(6)
