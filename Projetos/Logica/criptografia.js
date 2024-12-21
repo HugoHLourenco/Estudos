dc7db912a0cb57d3b70db1e49317f9fd30069688
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-21 17:23:53 -0300

    Update criptografia.js

--- Projetos/Logica/criptografia.js
+++ Projetos/Logica/criptografia.js
@@ -90,7 +90,6 @@ const criptogradiaAlfabeto = (string) => {
                 stringCripto.push(caracter[i])
                 break
             }
-            
         }
         
     console.log(`'${string}' criptografado é: ${stringCripto}`)
