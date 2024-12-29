b98e09e1220e6dee7bb1acbb0697175491963f6f
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-29 10:11:10 -0300

    Create teste.js

---
+++ Projetos/Logica/teste.js
@@ -0,0 +1,32 @@
+// NSoD {
+// 	    1x Necrotic Sword's Hilt {
+// 		    1x Energized Hilt {
+// 			    1x Bones Form the Void Realm
+// 			    1x Unenhanced hilt
+// 			    100x Void aura
+// 		    }
+// 		1x Bones from the VoidRealm
+// 		200x Void aura
+// 	    }
+// 	    1x Necrotic Sword's Aura {
+// 		    1x Energized Aura {
+// 			    1x Time Lord's Necronomicon {
+// 				    10x Chaorruptd Hourglass
+// 				    1x Necro scroll
+// 				    100x Void Aura
+// 			    }
+// 			1x Unenhanced Aura
+// 			150x Void aura
+// 		}
+
+// 		1x Time Lord's Necronomicon {
+// 			10x Chaorruptd Hourglass
+// 			1x Necro scroll
+// 			100x Void Aura
+// 		}
+
+// 		300x Void Aura
+// 	    }
+// 	    800x Void Aura
+// 	    1x Doom Heart
+// }
