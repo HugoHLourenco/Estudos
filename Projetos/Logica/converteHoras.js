86f0fb8ea60691b2543a0612618f9c99701002de
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-12-31 23:24:37 -0300

    Update converteHoras.js

---
+++ Projetos/Logica/converteHoras.js
@@ -0,0 +1,10 @@
+const converteHoras = (h, m, s)  => {
+    let horas = h * 360
+    let minutos = m * 60
+    let segundos = s
+    let soma = horas + minutos + segundos
+
+    console.log(`${h} horas, ${m} minutos e ${s} segundos são: ${soma} segundos`)
+}
+
+converteHoras(7, 48, 33)
