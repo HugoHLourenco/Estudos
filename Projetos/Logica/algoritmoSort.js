57e9da7e0deab0bbaf890cc1ec209e6bc5408c0d
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-01-04 17:00:43 -0300

    Update algoritmoSort.js

--- Projetos/Logica/algoritmoSort.js
+++ Projetos/Logica/algoritmoSort.js
@@ -5,22 +5,29 @@ const sort = (input) => {
         array[i] = input.charAt(i)
     }
     console.log(array)
+    
 // Manipular a string
 ordena()
 
 function ordena() {
-    for(let i = 0; array[i] > array[array.length - 1]; i++) {
+    let cont = 0
+    let  i = 0
+
+    for(i = 0; array[i] > array[array.length - 1]; i++) {
         if(array[i] > array[i + 1]) {
             let x = ""
             x = array[i + 1]
             array[i + 1] = array[i]
             array[i] = x
         }
+
+        cont ++
+
+        if (i > cont) {
+            ordena()
+        }
     }
 
-    if (array[0] > array[1] || array[array.length - 2] > array[array.length - 1]) {
-        ordena()
-    }
 }
 
     console.log(array)
