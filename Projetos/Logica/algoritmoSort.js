cb5f11c3ec93a203f7259b4f5f569df832d5594d
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-01-04 16:50:18 -0300

    Revert "teste"
    This reverts commit 0e638b34c6caefc499a1491014277f51a298a5c6.

--- Projetos/Logica/algoritmoSort.js
+++ Projetos/Logica/algoritmoSort.js
@@ -6,19 +6,22 @@ const sort = (input) => {
     }
     console.log(array)
 // Manipular a string
-function ordena() {
-    for(let i = 0; array[i] > array[array.length - 1]; i++) {
-        // if(array[i] > array[i + 1]) {
-        //     let x = ""
-        //     x = array[i + 1]
-        //     array[i + 1] = array[i]
-        //     array[i] = x
-        // }
-    }
-}
-
 ordena()
 
+function ordena() {
+    for(let i = 0; array[i] > array[array.length - 1]; i++) {
+        if(array[i] > array[i + 1]) {
+            let x = ""
+            x = array[i + 1]
+            array[i + 1] = array[i]
+            array[i] = x
+        }
+    }
+
+    if (array[0] > array[1] || array[array.length - 2] > array[array.length - 1]) {
+        ordena()
+    }
+}
 
     console.log(array)
 }
