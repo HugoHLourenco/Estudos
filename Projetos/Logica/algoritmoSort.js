0e638b34c6caefc499a1491014277f51a298a5c6
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-01-04 16:41:54 -0300

    teste

--- Projetos/Logica/algoritmoSort.js
+++ Projetos/Logica/algoritmoSort.js
@@ -6,22 +6,19 @@ const sort = (input) => {
     }
     console.log(array)
 // Manipular a string
-ordena()
-
 function ordena() {
     for(let i = 0; array[i] > array[array.length - 1]; i++) {
-        if(array[i] > array[i + 1]) {
-            let x = ""
-            x = array[i + 1]
-            array[i + 1] = array[i]
-            array[i] = x
-        }
-    }
-
-    if (array[0] > array[1] || array[array.length - 2] > array[array.length - 1]) {
-        ordena()
+        // if(array[i] > array[i + 1]) {
+        //     let x = ""
+        //     x = array[i + 1]
+        //     array[i + 1] = array[i]
+        //     array[i] = x
+        // }
     }
 }
+
+ordena()
+
 
     console.log(array)
 }
