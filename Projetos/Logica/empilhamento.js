0c8257b0343a9a01f3e58fed4736b3e690beeb9d
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-01-19 14:15:20 -0300

    Update empilhamento.js

--- Projetos/Logica/empilhamento.js
+++ Projetos/Logica/empilhamento.js
@@ -1,5 +1,12 @@
 const pilha = (base) => {
-    for(let i = base; base > 1; base --) {
-        
+    let res = 0
+    let basei = base
+
+    for(let i = base; base >= 1; base --) {
+        res += base * base
     }
+
+    console.log(`Uma pilha de base ${basei} tem ${res} objetos`)
 } 
+
+pilha(10)
