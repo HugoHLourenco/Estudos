a465826c79fb09f26532b85847bc70c09d96adbc
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-08-23 11:10:16 -0300

    Update calc_porcent.js

--- Projetos/Logica/calc_porcent.js
+++ Projetos/Logica/calc_porcent.js
@@ -1,9 +1,9 @@
 const calcPorcent = (a, b) => {
-    let valorDividido = a/b
+    let valorDividido = a / b
     let res = 100 * valorDividido
 
-    console.log(`${a} é ${res.toFixed(2)}% de ${b}`)
+    console.log(`${a} é ${Math.round(res * 100) / 100}% de ${b}`)
 }
 
 // a é primeiro número e b é o valor total
-calcPorcent(10, 37)
+calcPorcent(33, 40)
