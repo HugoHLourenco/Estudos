b9259ad2dde96f20c9b452e9a099f7dcc72ab1a3
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-10-02 18:13:23 -0300

    Update jokenpo.js

--- Projetos/Logica/jokenpo.js
+++ Projetos/Logica/jokenpo.js
@@ -1,3 +1,44 @@
 const jokenpo = (mao) =>{
-    
+    var randow = Math.floor(Math.random() * 3) + 1
+
+    switch(mao) {
+        case 1:
+        if(randow == 1) {
+            console.log(`Pedra contra Pedra\nEMPATE`)
+        } else if(randow == 2) {
+            console.log(`Pedra contra Papel\nDERROTA`)
+        } else if(randow == 3) {
+            console.log(`Pedra contra Tesoura\nVITÓRIA`)
+        } else {
+            console.log("Escolha um valor válido")
+        }
+        break
+
+        case 2:
+        if(randow == 1) {
+            console.log(`Papel contra Pedra\nVITÓRIA`)
+        } else if(randow == 2) {
+            console.log(`Papel contra Papel\nEMPATE`)
+        } else if(randow == 3) {
+            console.log(`Papel contra Tesoura\nDERROTA`)
+        } else {
+            console.log("Escolha um valor válido")
+        }
+        break
+
+        case 3:
+        if(randow == 1) {
+            console.log(`Tesoura contra Pedra\nDERROTA`)
+        } else if(randow == 2) {
+            console.log(`Tesoura contra Papel\nVITÓRIA`)
+        } else if(randow == 3) {
+            console.log(`Tesoura contra Tesoura\nEMPATE`)
+        } else {
+            console.log("Escolha um valor válido")
+        }
+        break
+    }
+
 }
+
+jokenpo(3)
