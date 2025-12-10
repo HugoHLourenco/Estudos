4b8f2bcc60fb4a082dd08d03789805478cceca99
Hugo H. Lourenço <hugohlourenco@gmail.com>
2025-12-10 20:54:41 -0300

    Update aaa.js

--- Projetos/Logica/aaa.js
+++ Projetos/Logica/aaa.js
@@ -1,5 +1,9 @@
-const func = () => {
-    
+const func = (n) => {
+    var stringRES = ""
+    for(i = 0; i <= n.length; i++){
+        stringRES = n[1]
+    }
+    console.log(stringRES)
 }
 
 func(234554)
