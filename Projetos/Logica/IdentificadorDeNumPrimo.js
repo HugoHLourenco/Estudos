ddfc09c3f7b0a06128ac9b313c8e994a43c4b5cd
Hugo H. Lourenço <hugohlourenco@gmail.com>
2026-08-21 10:52:17 -0300

    Update IdentificadorDeNumPrimo.js

--- Projetos/Logica/IdentificadorDeNumPrimo.js
+++ Projetos/Logica/IdentificadorDeNumPrimo.js
@@ -1,10 +1,13 @@
 function IdentificarNumPrimo(n) {
     let NumPrimos = [] // Vetor que armezena os Num. primos
 
-    for (i = 1; n >= i; i++) { // Laço que gera os primos
-        for (j = 0; j <= i; j++) {
-            if (n / i) {
-                NumPrimos.push(i)
+    for (i = 1; n > i; i++) { // Laço que gera os primos
+        if (n / i || n / 1) {
+            NumPrimos.push(i)
+            for (j = 0; j <= i; j++) {
+                if (NumPrimos[j] / i) {
+                    
+                }
             }
         }
     }
